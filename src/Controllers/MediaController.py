edcbd8c07c502c64123cb8a0db07a080047075a6
João Graf <grafjoaovitor@gmail.com>
2025-04-29 20:12:45 -0300

    descompctando arquivo ZIP ou RAR

--- src/Controllers/MediaController.py
+++ src/Controllers/MediaController.py
@@ -1,6 +1,9 @@
 import os
 import zipfile
+import rarfile
 from flask import request, jsonify
+
+rarfile.UNRAR_TOOL = r"C:\Program Files\UnRAR\UnRAR.exe"
 
 PASTA_MEDIA = "media"
 if not os.path.exists(PASTA_MEDIA):
@@ -14,20 +17,24 @@ def Upload():
     arquivo = request.files["arquivo"]
 
     if arquivo.filename == "":
-        return jsonify({"erro": "Nenhum arquivo selecionado."})
+        return jsonify({"erro": "Nenhum arquivo selecionado."}), 400
 
-    if not arquivo.filename.endswith(".zip"):
-        return jsonify({"erro": "O arquivo precisa ser um ZIP."}), 400
+    if not (arquivo.filename.endswith(".zip") or arquivo.filename.endswith(".rar")):
+        return jsonify({"erro": "O arquivo precisa ser um ZIP ou RAR."}), 400
 
     try:
-        zip_path = os.path.join(PASTA_MEDIA, arquivo.filename)
+        caminho_arquivo = os.path.join(PASTA_MEDIA, arquivo.filename)
+        arquivo.save(caminho_arquivo)
 
-        arquivo.save(zip_path)
+        if arquivo.filename.endswith(".zip"):
+            with zipfile.ZipFile(caminho_arquivo, "r") as zip_ref:
+                zip_ref.extractall(PASTA_MEDIA)
 
-        with zipfile.ZipFile(zip_path, "r") as zip_ref:
-            zip_ref.extractall(PASTA_MEDIA)
+        elif arquivo.filename.endswith(".rar"):
+            with rarfile.RarFile(caminho_arquivo, "r") as rar_ref:
+                rar_ref.extractall(PASTA_MEDIA)
 
-        os.remove(zip_path)
+        os.remove(caminho_arquivo)
 
         return jsonify({"sucesso": "Arquivo descompactado com sucesso!"}), 200
 
