5126cc8b23b64b5e160815877f8ae4f022756102
João Graf <grafjoaovitor@gmail.com>
2025-04-30 13:05:16 -0300

    install cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from src.Routes.routes import routes
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
